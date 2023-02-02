8e8da3f14c93d7181cf504397ab8f7b0e4c23d94
Nisargshah18 <nisargshah1852000@gmail.com>
2023-02-02 13:27:17 -0600

    Initial Commit

---
+++ ReadAssist/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ReadAssist
+//
+//  Created by Nisarg Shah on 2023-01-30.
+//
+
+import UIKit
+import Photos
+import PhotosUI
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var UIImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func ImagePicker(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker,animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else {return}
+        UIImageView.image = image
+        dismiss(animated: true)
+        
+    }
+}
